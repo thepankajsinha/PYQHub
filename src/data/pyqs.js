e23382b06f63a19756191e3c42257afe35dc2cf5
thepankajsinha <askpankajsinha@gmail.com>
2025-01-02 16:36:59 +0530

    update

--- src/data/pyqs.js
+++ src/data/pyqs.js
@@ -1,22 +1,67 @@
 const pyqData = [
   {
     id: 1,
-    year: 2023,
+    year: 2024,
     branch: "Computer Science",
-    subject: "Data Structures",
+    subject: "OOPS in C++",
     examtype: "End Semester Examination",
-    semester: "4th",
-    downloadUrl: "#"
+    semester: "3th",
+    downloadUrl: "https://drive.google.com/file/d/1h3AkZT9_DOgM7GEXB6yALQUFGPcQWrvd/view?usp=sharing"
   },
   {
     id: 2,
-    year: 2023,
-    branch: "Electrical",
-    subject: "Circuit Theory",
+    year: 2024,
+    branch: "Computer Science",
+    subject: "OOPS in C++",
     examtype: "Mid Semester Examination",
-    semester: "4th",
-    downloadUrl: "#"
-  }
+    semester: "3th",
+    downloadUrl: "https://drive.google.com/file/d/1hJn_7Heo-GRAehJktJuVCfMfZVSdNN7i/view?usp=sharing"
+  },
+  {
+    id: 3,
+    year: 2023,
+    branch: "Computer Science",
+    subject: "Indian Knowlege System",
+    examtype: "End Semester Examination",
+    semester: "3th",
+    downloadUrl: "https://drive.google.com/file/d/1hAn782dgizSKm0VRn-OCG3rmRW0EAyFq/view?usp=sharing"
+  },
+  {
+    id: 4,
+    year: 2023,
+    branch: "Computer Science",
+    subject: "Indian Knowlege System",
+    examtype: "Mid Semester Examination",
+    semester: "3th",
+    downloadUrl: "https://drive.google.com/file/d/1hQNb_yuIlJ_i5zlHy41txPmDbCgxqS_K/view?usp=sharing"
+  },
+  {
+    id: 5,
+    year: 2024,
+    branch: "Computer Science",
+    subject: "Data Structure",
+    examtype: "End Semester Examination",
+    semester: "3th",
+    downloadUrl: "https://drive.google.com/file/d/1h495v9sa-46NXA-HrESF72R1P1nkM7gO/view?usp=sharing"
+  },
+  {
+    id: 6,
+    year: 2023,
+    branch: "Computer Science",
+    subject: "Discrete Mathematics",
+    examtype: "Mid Semester Examination",
+    semester: "3th",
+    downloadUrl: "https://drive.google.com/file/d/1hJ-zEFHogzcKrqb_C-4J2Nd_wGVjgNUm/view?usp=sharing"
+  },
+  {
+    id: 7,
+    year: 2024,
+    branch: "Computer Science",
+    subject: "Computational Methods",
+    examtype: "End Semester Examination",
+    semester: "3th",
+    downloadUrl: "https://drive.google.com/file/d/1hALt46E2JRBW0kFKLDf5HNe9T-DLMrlt/view?usp=sharing"
+  },
 ];
 
 export default pyqData;
